0a1f6b053bc09fd9b5c358d70640c5ec3ab8aefa
Sarasakthi <saraswathi1404@gmail.com>
2023-11-09 19:07:02 -0700

    working on payment styles

--- src/payment.jsx
+++ src/payment.jsx
@@ -10,20 +10,22 @@ export default function Payment() {
     // Prevent the browser from reloading the page
     function handleSubmit(e) {
         e.preventDefault();
-    
+        //console.log("You have submitted:", this.state.selectedOption);
+
         // Read the form data
         const form = e.target;
         const formData = new FormData(form);
-    
+
+        /*
         // You can pass formData as a fetch body directly:
-        fetch('/some-api', { method: form.method, body: formData });
-    
+        fetch('/some-api', { method: form.method, body: formData });*/
+
         // Or you can work with it as a plain object:
         const formJson = Object.fromEntries(formData.entries());
         console.log(formJson);
-      }
+    }
 
-      //https://react.dev/reference/react-dom/components/input#caveats
+    //https://react.dev/reference/react-dom/components/input#caveats
 
     return (
         <form method="post" onSubmit={handleSubmit}>
@@ -33,49 +35,67 @@ export default function Payment() {
 
                 {/*Page Name*/}
                 <div className="paymentDiv">
-                    <p
-                        className="paymentHeader"
-                    /*style={{ fontFamily: 'Ubuntu' }}*/
-                    >Enter Payment Details</p>
+                    <p className="paymentHeader">Enter Payment Details</p>
                 </div>
 
                 <div className="paymentDetails">
-                    <ul>
-                        <li><label> Transfer From:
-                            <input type="text" name="paymentTransferFrom" /></label>
-                        </li>
+                    <div className="paymentFormGroup">
+                        
+                        <div className="paymentTransferFrom" id="paymentDiv">
+                            <label className="paymentLabel">Transfer From:
+                                <select>
+                                    <option value="Account1" id="Account1" >Checking Account</option>
+                                    <option value="Account2" id="Account2">Savings Account</option>
+                                    <option value="Account3" id="Account3">TFS Account</option>
+                                </select>
+                            </label>
+                        </div>
 
-                        <li><label> Transfer To:
-                            <input type="text" name="paymentTransferTo" /></label>
-                        </li>
+                        <div className="paymentTransferTo" id="paymentDiv">
+                        <label className="paymentLabel">Transfer From:
+                                <select>
+                                    <option value="beneficiary1" id="beneficiary1" >Beneficiary 1</option>
+                                    <option value="beneficiary2" id="beneficiary2">Beneficiary 2</option>
+                                    <option value="beneficiary3" id="beneficiary3">Beneficiary 3</option>
+                                    <option value="beneficiary4" id="beneficiary4">Beneficiary 4</option>
+                                    <option value="beneficiary5" id="beneficiary5">Beneficiary 5</option>
+                                </select>
+                            </label>
+                        </div>
 
-                        <li><label> Transfer Amount$:
-                            <input type="text" name="paymentAmount" /></label>
-                        </li>
+                        <div className="paymentAmount" id="paymentDiv">
+                            <label className="paymentLabel">Transfer Amount$:
+                                <input type="text" name="paymentNameAmount" id="paymentIDAmount" /></label>
+                        </div>
 
-                        <li><label> Remarks:
-                            <input type="text" name="paymentRemarks" /></label>
-                        </li>
+                        <div className="paymentRemarks" id="paymentDiv">
+                            <label className="paymentLabel">Remarks:
+                                <input type="text" name="paymentNameRemarks" id="paymentIDRemarks" /></label>
+                        </div>
 
-                        <li>
-                            <select>
-                                <option value="One Time">One Time</option>
-                                <option value="Recurring">Recurring</option>
-                            </select>
-                            <label>
-                                <input type="radio" name="freqOneTime" value="onetime" checked />One Time</label>
-                            <label>
-                                <input type="radio" name="freqRecurring" value="recurring" />Recurring</label>
-                        </li>
+                        <div className="paymentFrequency" id="paymentDiv">
+                            <label className="paymentLabel">Frequency:
+                                <div>
+                                    <input type="radio" name="paymentNameFrequency" id="paymentIDFreqOneTime"
+                                        value="onetime" />One Time
+                                    <input type="radio" name="paymentNameFrequency" id="paymentIDFreqRecurring"
+                                        value="recurring" />Recurring
+                                </div>
+                            </label>
+                        </div>
 
-                        <li>
-                            <input type="submit" name="paymentMakePayment" />
-                        </li>
-                    </ul>
 
+                        <div className="paymentMakePayment" id="paymentDiv">
+                            <button type="submit" name="paymentNameMakePayment" id="paymentIDMakePayment">
+                                Make Payment
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
+            </div>
 
+            <div className="footer">
                 <footer className="footerText">
                     {/*Insert Footer*/}
                     <Footer />
